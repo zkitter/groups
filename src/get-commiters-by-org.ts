6cc4e2d103a51e76c3a274954b87fc4fb0d35429
r1oga <38692952+r1oga@users.noreply.github.com>
2023-01-10 17:31:54 +0100

    get commits between 2 dates

--- src/get-commiters-by-org.ts
+++ src/get-commiters-by-org.ts
@@ -2,12 +2,24 @@ import { ok } from 'assert'
 import { URLS } from './constants'
 import committersQuery from './graphql/committers-query'
 
-export const getCommittersByOrg = async (org: string) => {
+export const getCommittersByOrg = async ({
+  org,
+  since,
+  until,
+}: {
+  org: string
+  since: Date
+  until: Date
+}) => {
   ok(process.env.GH_PAT, 'GH_PAT is not defined')
   const res = await fetch(URLS.GH_SQL, {
     body: JSON.stringify({
       query: committersQuery,
-      variables: { login: org },
+      variables: {
+        login: org,
+        since: since.toISOString(),
+        until: until.toISOString(),
+      },
     }),
     headers: {
       // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
@@ -22,13 +34,14 @@ export const getCommittersByOrg = async (org: string) => {
   return [
     ...new Set(
       (repos as any[])
-        .reduce<string[]>((repos, repo) => {
+        .reduce<Array<{ date: string; user: string }>>((repos, repo) => {
           // console.log(repo)
           if (repo.defaultBranchRef !== null) {
             repos.push(
-              repo.defaultBranchRef.target.history.nodes.map(
-                (node: any) => node.author.user.login,
-              ),
+              repo.defaultBranchRef.target.history.nodes.map((node: any) => ({
+                date: node.committedDate,
+                user: node.author.user.login,
+              })),
             )
           }
           return repos
