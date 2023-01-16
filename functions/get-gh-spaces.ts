c41d5f0c4a22dfb20c62b6eb831bbfd3a5607cbc
r1oga <38692952+r1oga@users.noreply.github.com>
2023-01-16 17:18:50 +0100

    feat: define serverless functions (#10)

---
+++ functions/get-gh-spaces.ts
@@ -0,0 +1,13 @@
+import 'cross-fetch/polyfill'
+import { Request, Response } from 'express'
+
+import { get100TopDaosWithMin10kFollowers } from '../src/get-spaces'
+
+export default async (_: Request, res: Response) => {
+  try {
+    const spaces = await get100TopDaosWithMin10kFollowers()
+    res.json(spaces)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+}
