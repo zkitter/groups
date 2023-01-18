46df44784da85a18ac71465c1324f6907018e2f8
r1oga <38692952+r1oga@users.noreply.github.com>
2023-01-18 11:06:54 +0000

    define serverless function

--- functions/daos/voters-group.ts
+++ functions/daos/voters-group.ts
@@ -1,11 +1,11 @@
 import 'cross-fetch/polyfill'
-import { Request, Response } from 'express'
+import { getVotersGroup } from 'daos/get-voters-group'
 
-import { getCommitersGroup } from 'gh/get-commiters-group'
+import { Request, Response } from 'express'
 
 export default async (_: Request, res: Response) => {
   try {
-    const group = await getCommitersGroup()
+    const group = await getVotersGroup()
     res.status(200).json(group)
   } catch (err) {
     res.status(500).json(err)
