9f166eb9f7724430141f8d628e9b1f28d90c129e
r1oga <38692952+r1oga@users.noreply.github.com>
2023-01-19 14:37:23 +0100

    test: add unit test for utils (#14)
    * remove snapshot serverless functions
    
    Do not consume unnecessary quota of 10 free serverless functions on nhost
    
    * refactor
    
    * test: add unit test for utils
    
    * fix minusOneMonth

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,8 @@
 export const minusOneMonth = (date: Date) =>
-  new Date(new Date().setMonth(date.getMonth() - 1))
+  new Date(new Date(date).setMonth(date.getMonth() - 1))
 
 export const getTime = (date: Date) => Math.floor(date.getTime() / 1000)
 
 export const ArraySet = <T>(array: T[]) => [...new Set(array)]
+
+export const notBot = (str: string) => !str.includes('[bot]')
---
+++ test/unit/utils.test.ts
@@ -0,0 +1,33 @@
+import { ArraySet, getTime, minusOneMonth, notBot } from 'utils'
+
+describe('utils', () => {
+  describe('ArraySet', () => {
+    it('should return a list of unique elements', () => {
+      const arr = ['a', 'b', 'c', 'a', 'b', 'c']
+      const set = ArraySet(arr)
+      expect(set).toEqual(['a', 'b', 'c'])
+    })
+  })
+
+  describe('minusOneMonth', () => {
+    it('should return a date one month ago', () => {
+      const date = new Date('2021-02-01')
+      const oneMonthAgo = new Date('2021-01-01')
+      expect(minusOneMonth(date)).toEqual(oneMonthAgo)
+    })
+  })
+
+  describe('getTime', () => {
+    it('should return number timestamp without milliseconds', () => {
+      const date = new Date('2021-01-01')
+      expect(getTime(date)).toEqual(1609459200)
+    })
+  })
+
+  describe('notBot', () => {
+    it('should return true if string does not contain [bot]', () => {
+      expect(notBot('a')).toBe(true)
+      expect(notBot('a[bot]')).toBe(false)
+    })
+  })
+})
