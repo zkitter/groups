17f54f900bfee0b7360c94b246a8c896258abe68
r1oga <38692952+r1oga@users.noreply.github.com>
2023-01-19 19:40:55 +0000

    update serverless functions

--- functions/daos/voters-group.ts
+++ functions/daos/voters-group.ts
@@ -1,13 +1,3 @@
 import 'cross-fetch/polyfill'
-import { getVotersGroup } from 'daos/get-voters-group'
 
-import { Request, Response } from 'express'
-
-export default async (_: Request, res: Response) => {
-  try {
-    const group = await getVotersGroup()
-    res.status(200).json(group)
-  } catch (err) {
-    res.status(500).json(err)
-  }
-}
+export { default } from 'handlers/voters-group'
