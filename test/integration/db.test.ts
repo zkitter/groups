cec657b355f276c7b1ed83ba3e35d750d0992231
r1oga <38692952+r1oga@users.noreply.github.com>
2023-01-27 15:13:26 +0000

    test prisma db connection

---
+++ test/integration/db.test.ts
@@ -0,0 +1,18 @@
+// import { Container } from 'typedi'
+import { Db } from 'db/prisma'
+import { orgBuilder } from '../lib'
+
+describe('Prisma (Mongo) DB', () => {
+  const db = new Db()
+
+  afterAll(async () => {
+    await db.org.deleteMany()
+    await db.prisma.$disconnect()
+  })
+
+  it('can create an org', async () => {
+    const org = orgBuilder()
+    const createdOrg = await db.prisma.org.create({ data: org })
+    expect(createdOrg).toMatchObject(org)
+  })
+})
