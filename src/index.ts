2625d2c18b4f8e65b237bb331ee634c3b9f259aa
r1oga <38692952+r1oga@users.noreply.github.com>
2023-01-30 12:08:13 +0000

    Update PORT

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,15 @@ import { app } from './app'
 
 const start = async () => {
   console.log('Starting...')
-  /*
-    type guard for env variables
-    in start function rather than route file
-    so that error is caught right at app start
-  */
+
   const { DATABASE_URL, GH_PAT } = process.env
+
   if (GH_PAT === undefined) throw new Error('GH_PAT not defined')
   if (DATABASE_URL === undefined) throw new Error('DATABASE_URL not defined')
 
-  app.listen(3000)
+  app.listen(8080)
 }
 
 start()
-  .then(() => console.log('👂 on port 3000'))
+  .then(() => console.log(`👂 on port 8080`))
   .catch((err) => console.error(err))
