399e55d1a76f3f998cc30e0158d313c578cf95dc
r1oga <38692952+r1oga@users.noreply.github.com>
2023-02-28 19:02:07 +0100

    feat:test: can check wether a user belongs to voters group

--- test/unit/user-service.test.ts
+++ test/unit/user-service.test.ts
@@ -1,6 +1,12 @@
 import { TestBed } from '@automock/jest'
-import { GithubRepository, MongoRepository } from 'repositories'
+import { faker } from '@faker-js/faker'
+import {
+  GithubRepository,
+  MongoRepository,
+  SnapshotRepository,
+} from 'repositories'
 import { UserService, WhitelistService } from 'services'
+import { getTime } from '../../src/utils'
 
 describe('UserService', () => {
   const REPOS = [
@@ -14,6 +20,7 @@ describe('UserService', () => {
   let userService: UserService
   let db: jest.Mocked<MongoRepository>
   let gh: jest.Mocked<GithubRepository>
+  let snapshot: jest.Mocked<SnapshotRepository>
   let whitelist: jest.Mocked<WhitelistService>
 
   beforeEach(() => {
@@ -21,10 +28,11 @@ describe('UserService', () => {
     userService = unit
     gh = unitRef.get(GithubRepository)
     db = unitRef.get(MongoRepository)
+    snapshot = unitRef.get(SnapshotRepository)
     whitelist = unitRef.get(WhitelistService)
   })
 
-  it('getContributedRepositories: fetches from GH and returns contributed repos', async () => {
+  it('getContributedRepos: fetches from GH and returns repos a user has contributed to', async () => {
     gh.getContributedRepos.mockResolvedValueOnce(REPOS)
 
     await expect(userService.getContributedRepos('foo')).resolves.toEqual([
@@ -34,9 +42,30 @@ describe('UserService', () => {
     expect(gh.getContributedRepos).toHaveBeenCalledOnceWith('foo')
   })
 
+  it('getVotedOrgs: fetches from Snapshot and returns orgs a user has voted to', async () => {
+    const SPACE_IDS = ['space1', 'space2']
+    const address = faker.finance.ethereumAddress()
+    const since = faker.date.past()
+    const until = faker.date.future()
+    snapshot.getVotedSpacesByAddress.mockResolvedValueOnce(SPACE_IDS)
+
+    await expect(
+      userService.getVotedOrgs({ address, since, until }),
+    ).resolves.toEqual(SPACE_IDS)
+
+    expect(snapshot.getVotedSpacesByAddress).toHaveBeenCalledOnceWith({
+      address,
+      since: getTime(since),
+      until: getTime(until),
+    })
+  })
+
   describe('belongsToContributorsGroup', () => {
     it('returns true if user has contributed to a whitelisted repo', async () => {
-      whitelist.getWhitelistShort.mockResolvedValueOnce(['A/a'])
+      whitelist.getWhitelistShort.mockResolvedValueOnce({
+        daos: [],
+        repos: ['A/a'],
+      })
 
       await expect(
         userService.belongsToGhContributorsGroup({
@@ -49,7 +78,10 @@ describe('UserService', () => {
     })
 
     it('returns false if user has not contributed to a whitelisted repo', async () => {
-      whitelist.getWhitelistShort.mockResolvedValueOnce(['C/a'])
+      whitelist.getWhitelistShort.mockResolvedValueOnce({
+        daos: [],
+        repos: ['C/a'],
+      })
 
       await expect(
         userService.belongsToGhContributorsGroup({
@@ -57,6 +89,40 @@ describe('UserService', () => {
           repos: ['A/a', 'B/b'],
         }),
       ).resolves.toBe(false)
+
+      expect(whitelist.getWhitelistShort).toHaveBeenCalledOnce()
+    })
+  })
+
+  describe('belongsToVotersGroup', () => {
+    it('returns true if user has voted to a whitelisted org', async () => {
+      whitelist.getWhitelistShort.mockResolvedValueOnce({
+        daos: ['A'],
+        repos: [],
+      })
+      snapshot.getVotedSpacesByAddress.mockResolvedValueOnce(['A'])
+
+      await expect(userService.belongsToVotersGroup('0x123')).resolves.toBe(
+        true,
+      )
+
+      expect(whitelist.getWhitelistShort).toHaveBeenCalledOnce()
+      expect(snapshot.getVotedSpacesByAddress).toHaveBeenCalledOnceWith({
+        address: '0x123',
+        since: expect.any(Number),
+        until: expect.any(Number),
+      })
+    })
+
+    it('returns false if user has not voted to a whitelisted org', async () => {
+      whitelist.getWhitelistShort.mockResolvedValueOnce({
+        daos: ['C'],
+        repos: [],
+      })
+
+      await expect(userService.belongsToVotersGroup('0x123')).resolves.toBe(
+        false,
+      )
 
       expect(whitelist.getWhitelistShort).toHaveBeenCalledOnce()
     })
@@ -74,29 +140,15 @@ describe('UserService', () => {
     })
   })
 
-  describe('getGroups', () => {
-    it('returns data about the groups the user is part of', async () => {
-      whitelist.getWhitelistShort.mockResolvedValueOnce(['A/a'])
-
-      await expect(
-        userService.getGroups({
-          ghName: 'foo',
-          repos: ['A/a', 'B/b'],
-        }),
-      ).resolves.toEqual({
-        belongsToGhContributorsGroup: true,
-      })
-
-      expect(whitelist.getWhitelistShort).toHaveBeenCalledOnce()
-    })
-  })
-
-  describe('getUser', () => {
+  describe('getGhUser', () => {
     it('can return user data in longer format', async () => {
       db.findUser.mockResolvedValueOnce(USER_DATA)
-      whitelist.getWhitelistShort.mockResolvedValueOnce(['A/a'])
+      whitelist.getWhitelistShort.mockResolvedValueOnce({
+        daos: [],
+        repos: ['A/a'],
+      })
 
-      await expect(userService.getUser('foo', 'long')).resolves.toEqual({
+      await expect(userService.getGhUser('foo', 'long')).resolves.toEqual({
         belongsToGhContributorsGroup: true,
         ...USER_DATA,
       })
